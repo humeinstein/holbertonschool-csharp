ef0c376651118dc1a02c73ec1ffb64cf358c369e
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-13 15:40:59 -0400

    INIT: c# to print given variables as expected

---
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine("Percent: " + percent.ToString("0.00%"));
+		Console.WriteLine("Currency: " + currency.ToString("C"));
+
+	}
+}

157d4136b4ffd161de6d7071434660359ba9bd48
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-14 16:01:17 -0400

    INIT: program prints all numbs from 0 to 98

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                 for (int hundred = 0; hundred <= 100; hundred++) {
+		     Console.WriteLine(String.Format("{0} = 0x{0:x}", hundred, hundred)); }
+        }
+    }
+}

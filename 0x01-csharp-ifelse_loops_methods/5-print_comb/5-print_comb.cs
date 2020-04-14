51bead583f16e9d79bb0789609aead8984d0570f
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-14 16:56:07 -0400

    C# program prints 00 to 99 using 1 loop 1 console.write

--- 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -7,9 +7,14 @@ namespace _5_print_comb
         static void Main(string[] args)
         {
 		int num = 0;
+		string status = "";
+
 
 		while (num < 100) {
-		      Console.WriteLine(String.Format("{0:d2}", num));
+		      Console.Write(String.Format("{0:d2}", num));
+		      if (num < 99) { status = ", "; }
+		      else { status = "\n"; }
+		      Console.Write(String.Format("{0}", status));
 		      num++;
 		      }
         }

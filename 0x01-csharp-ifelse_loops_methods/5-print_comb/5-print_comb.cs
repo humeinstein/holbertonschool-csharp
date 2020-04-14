5883a4522b98665861faeffabe6a789be5b2e9bf
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-14 16:43:45 -0400

    INIT: program prints 0-99

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		int num = 0;
+
+		while (num < 100) {
+		      Console.WriteLine(String.Format("{0:d2}", num));
+		      num++;
+		      }
+        }
+    }
+}

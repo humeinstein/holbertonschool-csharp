ece8df3ded446b4042feed61ed8de2e0dadfda2d
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-14 13:09:46 -0400

    INIT: program prints string about last number of random 4-digit number

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+	int last = number % 10;
+	string status = " ";
+
+	if (last < 6) { status = " and is less than 6 and not zero"; }
+	else if (last > 5) { status = " and is greater than 5"; }
+	else { status = " and is 0"; }
+
+	Console.WriteLine(String.Format("The last digit of {0} is {1}{2}", number, last, status));
+    }
+}

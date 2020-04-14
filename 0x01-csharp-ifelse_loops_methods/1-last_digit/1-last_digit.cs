5c878d15611476c1b39152a894a2542143feb1fe
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-14 13:18:30 -0400

    BUGFIX: zero -> 0

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -9,7 +9,7 @@ class Program
 	int last = number % 10;
 	string status = " ";
 
-	if (last < 6) { status = " and is less than 6 and not zero"; }
+	if (last < 6) { status = " and is less than 6 and not 0"; }
 	else if (last > 5) { status = " and is greater than 5"; }
 	else { status = " and is 0"; }
 

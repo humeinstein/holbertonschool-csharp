86bf287313b2de5f3bc48ae8105c79d4f6b9202d
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-14 23:57:57 -0400

    INIT: program prints number 1 -> 100 fizz buzz

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int iter = 1; iter <= 100; iter++) {
+                if (iter % 5 == 0) { Console.Write("Buzz"); }
+                else if (iter % 15 == 0) { Console.Write("FizzBuzz"); }
+                else if (iter % 3 == 0) { Console.Write("Fizz"); }
+                else { Console.Write(iter); }
+                Console.Write(" ");
+            }
+            Console.Write("\n");
+        }
+    }
+}

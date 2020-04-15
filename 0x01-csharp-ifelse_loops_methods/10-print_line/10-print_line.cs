cf10178bf864906ae32947641e00fa8249e4abbc
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-14 23:10:25 -0400

    INIT: method that draws a straight line in the terminal

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+class Line {
+    public static void PrintLine(int length) {
+        if (length < 1) { Console.Write("\n"); }
+        for (int iter = 0; iter < length; iter++) { Console.Write( "_"); }
+    }
+}

d702d97cb1401bf0dae59f97e659da29dad46fba
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-14 22:23:15 -0400

    INIT: method that prints last digit of a number

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Number {
+    public static int PrintLastDigit(int number) {
+        int end = 0;
+        if (number < 0) { number *= -1; }
+        end = number % 10;
+        Console.Write(end);
+        return (end);
+    }
+}

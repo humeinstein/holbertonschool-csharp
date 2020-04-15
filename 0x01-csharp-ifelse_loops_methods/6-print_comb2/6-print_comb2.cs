511f5aa691cadb1f01cd5b3635c7831305881dbd
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-14 21:16:33 -0400

    program prnts all possible combinations of two digits

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int comma = 0;
+            int num2 = 0;
+            for (int num1 = 0; num1 < 9; num1++) {
+                for (num2 = num1 + 1; num2 < 10; num2++) {
+                    if (comma == 1) {
+                        Console.Write(", ");}
+                    comma = 1;
+                    Console.Write(String.Format("{0}{1}", num1, num2));
+            }
+        }
+            Console.Write("\n");
+        }
+    }
+}

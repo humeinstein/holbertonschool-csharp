99283713dbe3b3b050c6860afd333a4ae336b73a
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-16 15:54:34 -0400

    INIT: method finds max int out of list collection

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List {
+    public static int MaxInteger(List<int> myList) {
+        if (myList.Count == 0) { Console.WriteLine("List is empty"); return -1; }
+        else {
+            myList.Sort();
+            int max = myList[myList.Count - 1];
+            return max;
+        }
+    }
+}

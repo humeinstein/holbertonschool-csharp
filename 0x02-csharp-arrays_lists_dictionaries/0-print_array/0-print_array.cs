1e06fbe27bee6912748015fde64cccdc12c6f95c
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-16 10:49:35 -0400

    INIT: class method creates adn prints array of int of given size

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Array {
+    public static int[] CreatePrint(int size) {
+        if (size < 0) {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        int[] retArray = new int[size];
+        for(int iter = 0; iter < size; iter++) { retArray[iter] = iter; }
+
+        Console.WriteLine(String.Join(" ", retArray));
+        return retArray;
+    }
+}

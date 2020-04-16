63d98343cd25f40e11b7771d0f209aef30a76571
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-16 17:22:26 -0400

    INIT: method doubles all values in dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary {
+
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict) {
+        
+        Dictionary<string, int> byTwo = new Dictionary<string, int>();
+
+        foreach (var valueKP in myDict) { byTwo.Add(valueKP.Key, (valueKP.Value * 2)); }
+        return (byTwo);
+    }
+}

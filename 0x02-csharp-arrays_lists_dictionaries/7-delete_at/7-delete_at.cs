a120e52e5acacb8555c73acfd4611e073c895230
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-16 16:38:14 -0400

    BUG: i -> I in string

--- 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 class List {
     public static List<int> DeleteAt(List<int> myList, int index) {
-        if (index < 0 || index >= myList.Count) { Console.WriteLine("index is out of range"); return (myList); }
+        if (index < 0 || index >= myList.Count) { Console.WriteLine("Index is out of range"); return (myList); }
         else {
             myList.Remove(myList[index]);
             return (myList);

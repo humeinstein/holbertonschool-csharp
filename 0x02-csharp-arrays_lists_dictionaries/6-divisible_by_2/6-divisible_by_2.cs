90a2ec16b85f3a692bd3c955e0d5b5a6071dc1a0
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-16 16:13:18 -0400

    INIT: method finds all multiples of 2 in a list

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List {
+    public static List<bool> DivisibleBy2(List<int> myList) {
+
+    
+        List<bool> byTwoList = new List<bool>();
+
+        foreach (var num in myList)
+        {
+            if (num % 2 == 0) { byTwoList.Add(true); }
+            else { byTwoList.Add(false); }
+        }
+
+        return byTwoList;
+    }
+}

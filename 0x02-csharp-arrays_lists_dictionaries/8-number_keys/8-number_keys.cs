b1e41012529fe7d5d3445195c7d8ce3484b27918
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-16 16:54:38 -0400

    INIT: method prints number of keys in a dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary {
+    public static int NumberOfKeys(Dictionary<string, string> myDict) {
+        int numKey = 0;
+        foreach (var key in myDict) { numKey++; }
+        return (numKey);
+    }
+}

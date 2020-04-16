3dab92937fbf0f4b7e147d873a9c2c9a7db960d3
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-16 16:59:23 -0400

    INIT: method adds key values and returns mydict

---
+++ 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+class Dictionary {
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value) {
+        myDict[key] = value;
+        return myDict;
+    }
+}

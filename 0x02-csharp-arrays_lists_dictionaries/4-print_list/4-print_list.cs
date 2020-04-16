fb2cac2d89d6c3181b5651d0b77f8d5d00e28cde
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-16 15:26:59 -0400

    INIT: method that creates and prints a list of integers of a given size

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+class List {
+    public static List<int> CreatePrint(int size) {
+        if (size < 0) { Console.WriteLine("Size cannot be negative"); return null; }
+    List<int> printList = new List<int>();
+    for (int iter = 0; iter < size; iter++) { printList.Add(iter); }
+    Console.WriteLine(string.Join(" ", printList));
+    return printList;
+
+    }
+}

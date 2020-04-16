fc52762dc7a3aaa212e942fc631c4fc1fd286e3e
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-16 11:31:47 -0400

    Method prints all intes of an array in reverse

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Array {
+    public static void Reverse(int[] array) {
+        if (array == null) { Console.WriteLine(""); return; }
+        else {
+            for (int iter = array.Length - 1; iter >= 0; iter--) {
+                Console.Write(array[iter]);
+                if (iter != 0) { Console.Write(" "); }
+            }
+            Console.Write("\n");
+        }
+    }
+}

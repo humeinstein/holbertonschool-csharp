3bda759ed06e6f824f68ea5f5200cd5031512240
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-16 20:02:05 -0400

    INIT: method prints a dictionary by ordered keys

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary {
+
+    public static void PrintSorted(Dictionary<string, string> myDict) {
+        SortedList<string, string> sortList = new SortedList<string, string>(myDict);
+
+        foreach (var keyVP in sortList.Keys) { Console.WriteLine("{0}:  {1}", keyVP, sortList[keyVP]); }
+    }
+}

9794926db6f4ae7a7214c7de573167073b3badb0
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-27 18:06:27 -0400

    INIT: return list of matching values of two given lists

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List {
+    public static List<int> CommonElements(List<int> list1, List<int> list2) {
+
+        List<int> matching = list1.FindAll(matchedValues => list2.Contains(matchedValues));
+        return matching;
+    }
+}

223af962ca49389691eca2f142ec48f944d153e9
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-27 18:29:15 -0400

    INIT: method returns sorted list of elements present in one lsit but not both

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List {
+    public static List<int> DifferentElements(List<int> list1, List<int> list2) {
+
+
+        List<int> new1 = list1.FindAll(elements => !list2.Contains(elements));
+        List<int> new2 = list2.FindAll(elements => !list1.Contains(elements));
+
+        new1.AddRange(new2);
+        new1.Sort();
+        return (new1);
+    }
+}

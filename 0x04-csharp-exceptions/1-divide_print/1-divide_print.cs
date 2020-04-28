b31dd3f241ab09c43d28fc3ccd209b569d363204
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-28 11:19:08 -0400

    INIT: method divide

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Int {
+
+    public static void divide(int a, int b) {
+
+        int amount = 0;
+        try {
+            amount = a / b;
+        }
+        catch {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally {
+            Console.WriteLine("{0} / {1} = {2}", a, b, amount);
+        }
+    }
+}

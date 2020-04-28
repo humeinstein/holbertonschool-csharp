cddaa7df2a667f1446faa8c1c80165df209b703e
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-27 22:20:49 -0400

    d

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList {
+
+    public static int GetNode(LinkedList<int> myLList, int n) {
+
+        int inter = 0;
+        foreach (var item in myLList)
+        {
+            if (iter == n) { return item; }
+            iter += 1;
+            
+        }
+        return 0;
+    }
+}

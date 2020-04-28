119b0396e180676796b679337b4c3a76d6fb904a
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-27 21:57:45 -0400

    INIT: method pop linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class LList {
+    public static int Pop(LinkedList<int> myLList) {
+
+        if (myLList.Count < 1) { return 0; }
+
+        LinkedListNode<int> firstN = myLList.First;
+        myLList.RemoveFirst();
+        return firstN.Value;
+    }
+}

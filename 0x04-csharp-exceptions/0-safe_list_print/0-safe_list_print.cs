a18369163955a3f0b33c924bf2815b31695dc6dd
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-28 11:14:50 -0400

    INIT: method safeprints try catch

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List {
+
+    public static int SafePrint(List<int> myList, int n) {
+
+        int amount = 0;
+        try {
+            for (int iter = 0; iter < n; iter++) {
+                Console.WriteLine(myList[iter]);
+                amount++;
+            }
+            return amount;
+        }
+        catch {
+            return amount;
+        }
+    }
+}

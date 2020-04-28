1ab144101e5efac7f65ca80825db9a02f2ac91a0
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-28 17:38:46 -0400

    INIT: add constructor to struct dog on param

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public enum Rating { Good, Great, Excellent }
+public struct Dog {
+    public string name;
+    public float age; 
+    public string owner;
+    public Rating rating;
+
+    public Dog(string dName, float dAge, string dOwner, Rating dR) {
+        name = dName;
+        age = dAge;
+        owner = dOwner;
+        rating = dR;
+    }
+}

b90ac97437bd571b72ee860ece79e7a6485bc365
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-28 13:51:41 -0400

    INIT: method divides values of 2 lists

--- 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -6,7 +6,7 @@ class List {
 
     int current = 0;
     List<int> dividedList = new List<int>();
-    
+
     foreach (var item in list1) {
 
         try { dividedList.Add(item / list2[current]); 
@@ -14,9 +14,7 @@ class List {
         catch (DivideByZeroException) { Console.WriteLine("Cannot divide by zero");
                                             dividedList.Add(0);
                                             current++ ; }
-        catch (ArgumentOutOfRangeException) { Console.WriteLine("Out of range");
-                                            dividedList.Add(0);
-                                            current++; }
+        catch (ArgumentOutOfRangeException) { Console.WriteLine("Out of range"); }
                                             
     }
     return dividedList;

225295b9f6c1a161254858c95ccc44902600c610
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-27 21:51:27 -0400

    INIT: method finds a value in linked list and returns its index position in the list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList {
+
+    public static int FindNode(LinkedList<int> myLList, int value) {
+
+        int check = 0;
+        foreach (var item in myLList)
+        {
+            if (item == value) { return check; }
+            check += 1;
+
+        }
+        return -1;
+    }
+}

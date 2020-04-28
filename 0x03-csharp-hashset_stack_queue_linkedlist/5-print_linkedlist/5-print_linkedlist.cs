3d8aeb9ac865de324750bf5363b8574d58c0fc7a
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-27 20:07:58 -0400

    INIT: method creates and prints lll of ints

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList {
+
+    public static LinkedList<int> CreatePrint(int size) {
+
+        LinkedList<int> llCollection = new LinkedList<int>();
+
+        for (int iter = 0; iter < size; iter++) { llCollection.AddLast(iter) ; } 
+        foreach (var item in llCollection) { Console.WriteLine(item) ; }
+        return  llCollection;
+    }
+}

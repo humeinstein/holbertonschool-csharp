b3032527eb1173236e4824cdf2b821e79da60aba
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-28 13:48:05 -0400

    INIT: method divides values of 2 lists

---
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List {
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength) {
+
+    int current = 0;
+    List<int> dividedList = new List<int>();
+    
+    foreach (var item in list1) {
+
+        try { dividedList.Add(item / list2[current]); 
+                current++ ; }
+        catch (DivideByZeroException) { Console.WriteLine("Cannot divide by zero");
+                                            dividedList.Add(0);
+                                            current++ ; }
+        catch (ArgumentOutOfRangeException) { Console.WriteLine("Out of range");
+                                            dividedList.Add(0);
+                                            current++; }
+                                            
+    }
+    return dividedList;
+    }
+}

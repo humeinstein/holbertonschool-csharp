90f12d7bf60ee2821d92301ec4abe98e0a5aa94a
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-28 17:42:34 -0400

    INIT working

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public enum Rating { Good, Great, Excellent }
+public struct Dog {
+    public string name;
+    public float age; 
+    public string owner;
+    public Rating rating;
+
+    public Dog(string dName, float dAge, string dOwner, Rating dR) {
+        name = dName;
+        age = dAge;
+        owner = dOwner;
+        rating = dR;
+    }
+    public override string ToString() {
+        return String.Format("Dog Name: {0}\nAge: {1}\nOwner: {2}\nRating: {3}", name, age, owner, rating);
+    }
+}

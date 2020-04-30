b8ab497554b1747a9098004eb877582d00b491d3
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-30 14:59:01 -0400

    INIT: class enemies has health now

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>comment test</summary>
+    public class Zombie {
+
+        ///<summary>public int health</summary>
+        public int health;
+
+        ///<summary>construction sets health at 0</summary>
+        public Zombie() { this.health = 0; }
+     }
+}

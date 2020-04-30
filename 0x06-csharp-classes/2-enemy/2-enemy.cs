31225e6f44088efbebd05093abad43113dfe5ed8
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-30 15:08:52 -0400

    INIT: add constructor to set value of health

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>comment test</summary>
+    public class Zombie {
+
+        ///<summary>public int health</summary>
+        public int health;
+
+        ///<summary>construction sets health at 0</summary>
+        public Zombie() { this.health = 0; }
+
+        ///<summary>constructor takes value as health</summary>
+        public Zombie(int value) {
+            if (value < 0) { throw new ArgumentException("Health must be greater than or equal to 0"); }
+
+            else { this.health = value ; }
+        }
+     }
+}

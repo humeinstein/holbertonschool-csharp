4033bbc2107a61e9624f752d98bf103b8ea3e504
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-30 15:22:26 -0400

    INIT: added GetHealth method

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>comment test</summary>
+    public class Zombie {
+
+        ///<summary>private int health</summary>
+        public int health;
+
+        ///<summary>construction sets health at 0</summary>
+        public Zombie() { this.health = 0; }
+
+        ///<summary>constructor takes value as health</summary>
+        public Zombie(int value) {
+            if (value < 0) { throw new ArgumentException("Health must be greater than or equal to 0"); }
+
+            else { this.health = value ; }
+
+        }
+
+         public int GetHealth() {
+            return this.health;
+        }
+
+     }
+}

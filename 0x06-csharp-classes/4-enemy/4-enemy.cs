1fcf6aef7f221dafa730686ff9a107c9d982cc94
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-04-30 21:18:11 -0400

    INIT: getter and setter OOP

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>comment test</summary>
+    public class Zombie {
+
+        ///<summary>private int health</summary>
+        private int health;
+
+        ///<summary>construction sets health at 0</summary>
+        public Zombie() { this.health = 0; }
+
+        ///<summary>constructor takes value as health</summary>
+        public Zombie(int value) {
+            if (value < 0) { throw new ArgumentException("Health must be greater than or equal to 0"); }
+
+            else { this.health = value ; }
+
+        }
+        ///<summary>name of zombie</summary>
+        private string name = "(No name)";
+        
+        ///<summary>getter and setter for name</summary>
+        public string Name {
+            get { return name; }
+            set { this.name = value;}
+        }
+
+        ///<summary>getHealth Method returns health of current zombie requesting</summary>
+         public int GetHealth() {
+            return this.health;
+        }
+
+     }
+}

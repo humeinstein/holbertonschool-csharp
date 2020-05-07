5ed9d0b32dfb51bd44e2be4cc98a4ce793337982
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-05-07 18:42:15 -0400

    INIT: bool is int

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+///<summary>Class</summary>
+class Obj {
+    ///<summary>is it an int?</summary>
+    public static bool IsOfTypeInt(object obj) {
+        return obj is int;
+    }
+}

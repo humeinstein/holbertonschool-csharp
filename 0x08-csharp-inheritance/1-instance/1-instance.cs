29d486b7093bbf6745e4751f3a66f9ff065123b0
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-05-07 18:57:09 -0400

    INIT: bool if in list

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+///<summary></summary>
+class Obj {
+    ///<summary></summary>
+    public static bool IsInstanceOfArray(object obj) {
+        return (typeof(Array).IsInstanceOfType(obj));
+    }
+}

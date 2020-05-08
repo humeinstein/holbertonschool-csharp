10f8d86547f6eb0b7a19a91a38a022ebcc7c6158
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-05-07 22:01:36 -0400

    Update 2-sublcass.cs

--- 0x08-csharp-inheritance/2-sublcass/2-sublcass.cs
+++ 0x08-csharp-inheritance/2-sublcass/2-sublcass.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 
+///<summary>class</summary>
 class Obj {
+
+    ///<summary>bool - sublass</summary>
     public static bool IsOnlyASubclass(Type derivedType, Type baseType) {
         return derivedType.IsSubclassOf(baseType);
     }

3bfd7b71659805ee72d44e5e630f8908bd0e4ec6
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-05-07 20:16:10 -0400

    INIT: print methods and props

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+
+///<summary>class</summary>
+class Obj {
+    ///<summary>class</summary>
+    public static void Print(object myObj) {
+        TypeInfo t = myObj.GetType().GetTypeInfo();
+        Console.WriteLine("{0} Properties:", t.Name);
+        foreach (PropertyInfo proInfo in t.GetProperties()) {
+            Console.WriteLine(proInfo.Name);
+        }
+        Console.WriteLine("{0} Methods:", t.Name);
+        foreach (MethodInfo Pinfo in t.GetMethods()) {
+            Console.WriteLine(Pinfo.Name);
+        }
+    }
+
+}

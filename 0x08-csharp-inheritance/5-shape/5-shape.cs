794f5cd343b07e92f16c0f5a25c6d74fea2b857f
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-05-07 23:04:12 -0400

    check

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+///<summary>class</summary>
+class Shape {
+    ///<summary>class</summary>
+    public virtual int Area() => throw new NotImplementedException("Area() is not implemented");
+}

e3da2421a18efda8da3f4ad7b2b8e6d48b9eada8
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-05-07 23:53:08 -0400

    working

--- 0x08-csharp-inheritance/8-shape/8-shape.cs
+++ 0x08-csharp-inheritance/8-shape/8-shape.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 
-namespace _8_shape
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+///<summary>class</summary>
+class Shape {
+    ///<summary>class</summary>
+    public virtual int Area() => throw new NotImplementedException("Area() is not implemented");
+}
+
+class Rectangle: Shape {
+    private int width;
+    private int height;
+
+    ///<summary>getter setter</summary>
+    public int Width {
+        get { return width; }
+        set {
+            if (value < 0) { throw new ArgumentException("Width must be greater than or equal to 0") ; }
+            else { width = value; }
+        }
+    }
+    public int Height {
+        get { return height; }
+        set {
+            if (value < 0) { throw new ArgumentException("Height must be greater than or equal to 0") ; }
+            else { height = value; }
+        }
+    }
+    public new int Area() {
+        return width * height;
+    }
+    public override string ToString() {
+        string str = "[Rectangle] " + width + " / " + height;
+        return str;
+    }
+}
+
+class Square : Rectangle {
+    private int size;
+    public int size {
+        get { return size; }
+        set {
+            if (value < 0) { throw new ArgumentException("Size must be greater than or equal to 0"); }
+            else { size = Height = Width = value; }
         }
     }
 }

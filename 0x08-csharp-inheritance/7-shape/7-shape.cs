c536c7788988159ac634cd0c060a6817585ac06e
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-05-07 23:44:38 -0400

    working

---
+++ 0x08-csharp-inheritance/7-shape/7-shape.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+///<summary>class</summary>
+class Shape {
+    ///<summary>class</summary>
+    public virtual int Area() => throw new NotImplementedException("Area() is not implemented");
+}
+
+class Rectangle: Shape {
+    private int width;
+    private int height;
+
+    ///<summary>getter setter</summary>
+    public int Width {
+        get { return width; }
+        set {
+            if (value < 0) { throw new ArgumentException("Width must be greater than or equal to 0") ; }
+            else { width = value; }
+        }
+    }
+    public int Height {
+        get { return height; }
+        set {
+            if (value < 0) { throw new ArgumentException("Height must be greater than or equal to 0") ; }
+            else { height = value; }
+        }
+    }
+    public override string ToString() {
+        string str = "[Rectangle] " + width + " / " + height;
+        return str;
+    }
+}
